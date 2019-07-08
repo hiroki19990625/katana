bb8ab3eb27e8146da03b52b0182ecc8bba67a753
hiroki19990625 <hiroki19990625@gmail.com>
2019-07-08 23:06:41 +0900

    Channelを定義

--- src/main/kotlin/jp/katana/server/network/packet/mcpe/MinecraftPacket.kt
+++ src/main/kotlin/jp/katana/server/network/packet/mcpe/MinecraftPacket.kt
@@ -4,6 +4,17 @@ import jp.katana.core.network.packet.mcpe.IMinecraftPacket
 import jp.katana.server.utils.BinaryStream
 
 abstract class MinecraftPacket : BinaryStream(), IMinecraftPacket, Cloneable {
+    companion object {
+        const val channel_none = 0
+        const val channel_priority = 1
+        const val channel_chunk = 2
+        const val channel_movement = 3
+        const val channel_blocks = 4
+        const val channel_events = 5
+        const val channel_spawning = 6
+        const val channel_text = 7
+    }
+
     override val packetId: Int = 0
     override val channel: Int = 0
 
