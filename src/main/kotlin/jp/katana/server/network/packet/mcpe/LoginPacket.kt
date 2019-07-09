2003500339df5514fe0fe3ec3f1e9241425dcfab
hiroki19990625 <hiroki19990625@gmail.com>
2019-07-10 02:41:49 +0900

    コメントアウトを解除

--- src/main/kotlin/jp/katana/server/network/packet/mcpe/LoginPacket.kt
+++ src/main/kotlin/jp/katana/server/network/packet/mcpe/LoginPacket.kt
@@ -7,6 +7,7 @@ import jp.katana.server.data.LoginData
 import jp.katana.server.utils.BinaryStream
 import org.apache.logging.log4j.LogManager
 import java.nio.charset.Charset
+import java.util.*
 
 class LoginPacket : MinecraftPacket() {
     override val packetId: Int = MinecraftProtocols.loginPacket
@@ -31,7 +32,7 @@ class LoginPacket : MinecraftPacket() {
 
         val clientLen = stream.readIntLE()
         val client = stream.read(clientLen)
-        //loginData.decode(String(Base64.getDecoder().decode(client), Charset.forName("utf8")))
+        clientData.decode(String(Base64.getDecoder().decode(client), Charset.forName("utf8")))
     }
 
     override fun encodePayload() {
