4ae62b84f4182a863f69603bc5405292d8658253
hiroki19990625 <hiroki19990625@gmail.com>
2019-07-26 02:03:03 +0900

    Positionにup,down,left,right

--- src/main/kotlin/jp/katana/server/math/BlockPosition.kt
+++ src/main/kotlin/jp/katana/server/math/BlockPosition.kt
@@ -3,4 +3,23 @@ package jp.katana.server.math
 import jp.katana.core.world.IWorld
 
 class BlockPosition(val world: IWorld, val pos: Vector3Int) {
+    fun up(step: Int): BlockPosition {
+        val n = pos + Vector3Int(0, step, 0)
+        return BlockPosition(world, n)
+    }
+
+    fun down(step: Int): BlockPosition {
+        val n = pos + Vector3Int(0, -step, 0)
+        return BlockPosition(world, n)
+    }
+
+    fun left(step: Int): BlockPosition {
+        val n = pos + Vector3Int(-step, 0, 0)
+        return BlockPosition(world, n)
+    }
+
+    fun right(step: Int): BlockPosition {
+        val n = pos + Vector3Int(step, 0, 0)
+        return BlockPosition(world, n)
+    }
 }
--- src/main/kotlin/jp/katana/server/math/Position.kt
+++ src/main/kotlin/jp/katana/server/math/Position.kt
@@ -3,5 +3,23 @@ package jp.katana.server.math
 import jp.katana.core.world.IWorld
 
 class Position(val world: IWorld, val pos: Vector3) {
+    fun up(step: Double): Position {
+        val n = pos + Vector3(0.0, step, 0.0)
+        return Position(world, n)
+    }
 
+    fun down(step: Double): Position {
+        val n = pos + Vector3(0.0, -step, 0.0)
+        return Position(world, n)
+    }
+
+    fun left(step: Double): Position {
+        val n = pos + Vector3(-step, 0.0, 0.0)
+        return Position(world, n)
+    }
+
+    fun right(step: Double): Position {
+        val n = pos + Vector3(step, 0.0, 0.0)
+        return Position(world, n)
+    }
 }
