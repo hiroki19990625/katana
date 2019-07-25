7e09cd10a44766ac1f2a6a7be968ba66cbf0aeea
hiroki19990625 <hiroki19990625@gmail.com>
2019-07-26 02:07:59 +0900

    Positionにup,down,left,rightを使いやすく

--- src/main/kotlin/jp/katana/server/math/BlockPosition.kt
+++ src/main/kotlin/jp/katana/server/math/BlockPosition.kt
@@ -3,9 +3,17 @@ package jp.katana.server.math
 import jp.katana.core.world.IWorld
 
 class BlockPosition(val world: IWorld, val pos: Vector3Int) {
+    fun up(): BlockPosition {
+        return up(1)
+    }
+
     fun up(step: Int): BlockPosition {
         val n = pos + Vector3Int(0, step, 0)
         return BlockPosition(world, n)
+    }
+
+    fun down(): BlockPosition {
+        return down(1)
     }
 
     fun down(step: Int): BlockPosition {
@@ -13,9 +21,17 @@ class BlockPosition(val world: IWorld, val pos: Vector3Int) {
         return BlockPosition(world, n)
     }
 
+    fun left(): BlockPosition {
+        return left(1)
+    }
+
     fun left(step: Int): BlockPosition {
         val n = pos + Vector3Int(-step, 0, 0)
         return BlockPosition(world, n)
+    }
+
+    fun right(): BlockPosition {
+        return right(1)
     }
 
     fun right(step: Int): BlockPosition {
--- src/main/kotlin/jp/katana/server/math/Position.kt
+++ src/main/kotlin/jp/katana/server/math/Position.kt
@@ -3,9 +3,17 @@ package jp.katana.server.math
 import jp.katana.core.world.IWorld
 
 class Position(val world: IWorld, val pos: Vector3) {
+    fun up(): Position {
+        return up(1.0)
+    }
+
     fun up(step: Double): Position {
         val n = pos + Vector3(0.0, step, 0.0)
         return Position(world, n)
+    }
+
+    fun down(): Position {
+        return down(1.0)
     }
 
     fun down(step: Double): Position {
@@ -13,9 +21,17 @@ class Position(val world: IWorld, val pos: Vector3) {
         return Position(world, n)
     }
 
+    fun left(): Position {
+        return left(1.0)
+    }
+
     fun left(step: Double): Position {
         val n = pos + Vector3(-step, 0.0, 0.0)
         return Position(world, n)
+    }
+
+    fun right(): Position {
+        return right(1.0)
     }
 
     fun right(step: Double): Position {
