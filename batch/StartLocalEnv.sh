3078935d01f6a476dccc1f2ce423036d20cf2861
hiroki19990625 <hiroki19990625@gmail.com>
2019-07-27 01:33:54 +0900

    shファイルの修正

--- batch/StartLocalEnv.sh
+++ batch/StartLocalEnv.sh
@@ -2,6 +2,6 @@
 cd ../
 mkdir local_env
 ./gradlew jar
-cp "build\libs\katana-1.0-SNAPSHOT.jar" "local_env\katana.jar"
+cp "build/libs/katana-1.0-SNAPSHOT.jar" "local_env/katana.jar"
 cd local_env
 java -jar katana.jar
