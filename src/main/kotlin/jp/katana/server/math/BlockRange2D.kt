9339a87120500054433f124e0c3f297439b3a7a7
hiroki19990625 <hiroki19990625@gmail.com>
2019-07-26 14:33:15 +0900

    containsを実装

--- src/main/kotlin/jp/katana/server/math/BlockRange2D.kt
+++ src/main/kotlin/jp/katana/server/math/BlockRange2D.kt
@@ -1,4 +1,5 @@
 package jp.katana.server.math
 
 data class BlockRange2D(val pos1: Vector2Int, val pos2: Vector2Int) {
+
 }
--- src/main/kotlin/jp/katana/server/math/BoundingBox2D.kt
+++ src/main/kotlin/jp/katana/server/math/BoundingBox2D.kt
@@ -1,4 +1,24 @@
 package jp.katana.server.math
 
-data class BoundingBox2D(val pos1: Vector2, val pos2: Vector2) {
+import kotlin.math.max
+import kotlin.math.min
+
+data class BoundingBox2D(val pos: Vector2, val bound: Vector2) {
+    fun contains(contain: Vector2): Boolean {
+        val sum = pos + bound
+        val maxX = max(sum.x, pos.x)
+        val maxY = max(sum.y, pos.y)
+        val minX = min(sum.x, pos.x)
+        val minY = min(sum.y, pos.y)
+
+        if (minX <= contain.x &&
+            minY <= contain.y &&
+            maxX >= contain.x &&
+            maxY >= contain.y
+        ) {
+            return true
+        }
+
+        return false
+    }
 }
