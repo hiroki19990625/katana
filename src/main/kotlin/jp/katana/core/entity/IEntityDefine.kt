1c87bf8b47980abc1f488d7c04bf392ec3c3f036
hiroki19990625 <hiroki19990625@gmail.com>
2019-07-30 01:31:30 +0900

    EntityDefine

--- src/main/kotlin/jp/katana/core/entity/IEntityDefine.kt
+++ src/main/kotlin/jp/katana/core/entity/IEntityDefine.kt
@@ -1,5 +1,10 @@
 package jp.katana.core.entity
 
 interface IEntityDefine {
-
+    val hasSpawnEgg: Boolean
+    val experimental: Boolean
+    val summonable: Boolean
+    val id: String
+    val bid: String
+    val rid: Int
 }
