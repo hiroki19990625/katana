488392797b1cf2741f90c9b22c6fabdf7c222961
hiroki19990625 <hiroki19990625@gmail.com>
2019-08-01 00:42:02 +0900

    スクリプトを更新

--- batch/Publish.sh
+++ batch/Publish.sh
@@ -11,6 +11,9 @@ if [ ! -d "$git_dir" ]; then
     git clone "https://github.com/hiroki19990625/maven-repo.git"
 fi
 
+git config --global user.email "ci_user@ci.com"
+git config --global user.name "hiroki19990625"
+
 git pull
 git add .
 git commit -m "publish repo"
