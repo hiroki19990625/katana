d5c46d1234a28a3896b51eff2e17feec970253e8
hiroki19990625 <hiroki19990625@gmail.com>
2019-11-02 01:25:17 +0900

    ブロックのステートデータをソート

--- src/main/kotlin/jp/katana/core/block/IBlockDefinitions.kt
+++ src/main/kotlin/jp/katana/core/block/IBlockDefinitions.kt
@@ -1,9 +1,11 @@
 package jp.katana.core.block
 
+import jp.katana.nbt.tag.INamedTag
+
 interface IBlockDefinitions {
     fun fromRuntime(runtimeId: Int): IBlockDefine
     fun fromId(id: Int): IBlockDefine
-    fun fromIdAndData(id: Int, data: Int): IBlockDefine
+    fun fromIdAndStates(id: Int, states: MutableMap<String, INamedTag>): IBlockDefine
     fun fromName(name: String): IBlockDefine
 
     fun register(blockDefine: IBlockDefine)
--- src/main/kotlin/jp/katana/server/block/BlockDefine.kt
+++ src/main/kotlin/jp/katana/server/block/BlockDefine.kt
@@ -1,10 +1,12 @@
 package jp.katana.server.block
 
 import jp.katana.core.block.IBlockDefine
+import jp.katana.nbt.tag.INamedTag
 
 class BlockDefine(
     override val runtimeId: Int,
     override val name: String,
     override val id: Short,
-    override val data: Short
+    override val version: Int,
+    override val states: MutableMap<String, INamedTag>
 ) : IBlockDefine
