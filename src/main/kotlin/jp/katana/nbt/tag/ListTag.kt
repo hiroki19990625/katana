c4385322831e084e2b7c4732b37bd1a08d2477ab
hiroki19990625 <hiroki19990625@gmail.com>
2019-11-04 21:10:31 +0900

    listTagのAPIを改善

--- src/main/kotlin/jp/katana/nbt/tag/ListTag.kt
+++ src/main/kotlin/jp/katana/nbt/tag/ListTag.kt
@@ -15,64 +15,218 @@ class ListTag(override var name: String, listType: Byte = INamedTag.BYTE) : INam
             false
     }
 
-    fun getByte(index: Int): ByteTag {
+    fun addBoolean(value: Boolean): Boolean {
+        return if (type == INamedTag.BYTE)
+            list.add(ByteTag("", if (value) 1 else 0))
+        else
+            false
+    }
+
+    fun addByte(value: Byte): Boolean {
+        return if (type == INamedTag.BYTE)
+            list.add(ByteTag("", value))
+        else
+            false
+    }
+
+    fun addShort(value: Short): Boolean {
+        return if (type == INamedTag.SHORT)
+            list.add(ShortTag("", value))
+        else
+            false
+    }
+
+    fun addInt(value: Int): Boolean {
+        return if (type == INamedTag.INT)
+            list.add(IntTag("", value))
+        else
+            false
+    }
+
+    fun addLong(value: Long): Boolean {
+        return if (type == INamedTag.LONG)
+            list.add(LongTag("", value))
+        else
+            false
+    }
+
+    fun addFloat(value: Float): Boolean {
+        return if (type == INamedTag.FLOAT)
+            list.add(FloatTag("", value))
+        else
+            false
+    }
+
+    fun addDouble(value: Double): Boolean {
+        return if (type == INamedTag.DOUBLE)
+            list.add(DoubleTag("", value))
+        else
+            false
+    }
+
+    fun addString(value: String): Boolean {
+        return if (type == INamedTag.STRING)
+            list.add(StringTag("", value))
+        else
+            false
+    }
+
+    fun addByteArray(value: Array<Byte>): Boolean {
+        return if (type == INamedTag.BYTE_ARRAY)
+            list.add(ByteArrayTag("", value))
+        else
+            false
+    }
+
+    fun addByteArray(value: ByteArray): Boolean {
+        return if (type == INamedTag.BYTE_ARRAY)
+            list.add(ByteArrayTag("", value.toTypedArray()))
+        else
+            false
+    }
+
+    fun addIntArray(value: Array<Int>): Boolean {
+        return if (type == INamedTag.INT_ARRAY)
+            list.add(IntArrayTag("", value))
+        else
+            false
+    }
+
+    fun addIntArray(value: IntArray): Boolean {
+        return if (type == INamedTag.INT_ARRAY)
+            list.add(IntArrayTag("", value.toTypedArray()))
+        else
+            false
+    }
+
+    fun addLongArray(value: Array<Long>): Boolean {
+        return if (type == INamedTag.LONG_ARRAY)
+            list.add(LongArrayTag("", value))
+        else
+            false
+    }
+
+    fun addLongArray(value: LongArray): Boolean {
+        return if (type == INamedTag.LONG_ARRAY)
+            list.add(LongArrayTag("", value.toTypedArray()))
+        else
+            false
+    }
+
+    fun getBoolean(index: Int): Boolean {
+        return getByte(index) == 1.toByte()
+    }
+
+    fun getByteTag(index: Int): ByteTag {
         return list[index] as ByteTag
     }
 
-    fun getShort(index: Int): ShortTag {
+    fun getByte(index: Int): Byte {
+        return getByteTag(index).value
+    }
+
+    fun getShortTag(index: Int): ShortTag {
         return list[index] as ShortTag
     }
 
-    fun getInt(index: Int): IntTag {
+    fun getShort(index: Int): Short {
+        return getShortTag(index).value
+    }
+
+    fun getIntTag(index: Int): IntTag {
         return list[index] as IntTag
     }
 
-    fun getLong(index: Int): LongTag {
+    fun getInt(index: Int): Int {
+        return getIntTag(index).value
+    }
+
+    fun getLongTag(index: Int): LongTag {
         return list[index] as LongTag
     }
 
-    fun getFloat(index: Int): FloatTag {
+    fun getLong(index: Int): Long {
+        return getLongTag(index).value
+    }
+
+    fun getFloatTag(index: Int): FloatTag {
         return list[index] as FloatTag
     }
 
-    fun getDouble(index: Int): DoubleTag {
+    fun getFloat(index: Int): Float {
+        return getFloatTag(index).value
+    }
+
+    fun getDoubleTag(index: Int): DoubleTag {
         return list[index] as DoubleTag
     }
 
-    fun getByteArray(index: Int): ByteArrayTag {
+    fun getDouble(index: Int): Double {
+        return getDoubleTag(index).value
+    }
+
+    fun getByteArrayTag(index: Int): ByteArrayTag {
         return list[index] as ByteArrayTag
     }
 
-    fun getString(index: Int): StringTag {
+    fun getByteArray(index: Int): Array<Byte> {
+        return getByteArrayTag(index).value
+    }
+
+    fun getStringTag(index: Int): StringTag {
         return list[index] as StringTag
     }
 
-    fun getList(index: Int): ListTag {
+    fun getString(index: Int): String {
+        return getStringTag(index).value
+    }
+
+    fun getListTag(index: Int): ListTag {
         return list[index] as ListTag
     }
 
-    fun getCompound(index: Int): CompoundTag {
+    fun getCompoundTag(index: Int): CompoundTag {
         return list[index] as CompoundTag
     }
 
-    fun getIntArray(index: Int): IntArrayTag {
+    fun getIntArrayTag(index: Int): IntArrayTag {
         return list[index] as IntArrayTag
     }
 
-    fun getLongArray(index: Int): LongArrayTag {
+    fun getIntArray(index: Int): Array<Int> {
+        return getIntArrayTag(index).value
+    }
+
+    fun getLongArrayTag(index: Int): LongArrayTag {
         return list[index] as LongArrayTag
+    }
+
+    fun getLongArray(index: Int): Array<Long> {
+        return getLongArrayTag(index).value
+    }
+
+    fun getTag(index: Int): INamedTag {
+        return list[index]
+    }
+
+    fun getAllTag(): Array<INamedTag> {
+        return list.toTypedArray()
+    }
+
+    fun removeTag(tag: INamedTag): Boolean {
+        return list.remove(tag)
+    }
+
+    fun removeTag(index: Int) {
+        list.removeAt(index)
     }
 
     operator fun get(index: Int): INamedTag {
         return list[index]
     }
 
-    fun getAll(): Array<INamedTag> {
-        return list.toTypedArray()
-    }
-
-    fun remove(tag: INamedTag): Boolean {
-        return list.remove(tag)
+    operator fun set(index: Int, tag: INamedTag) {
+        list[index] = tag
     }
 
     fun size(): Int {
