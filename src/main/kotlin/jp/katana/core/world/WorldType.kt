7011bfd1e3fa0efb8cabc88a218a8d288b6d26db
hiroki19990625 <hiroki19990625@gmail.com>
2019-11-11 14:28:18 +0900

    リファクタリング

--- src/main/kotlin/jp/katana/core/world/WorldType.kt
+++ src/main/kotlin/jp/katana/core/world/WorldType.kt
@@ -4,7 +4,8 @@ enum class WorldType {
     Default,
     Flat,
     Empty,
-    Customize
+    Customize,
+    Old
 }
 
 fun String.toWorldType(): WorldType {
@@ -13,16 +14,47 @@ fun String.toWorldType(): WorldType {
         "flat" -> WorldType.Flat
         "custom" -> WorldType.Customize
         "empty" -> WorldType.Empty
+        "old" -> WorldType.Old
         else -> WorldType.valueOf(this)
     }
 }
 
 fun String.toWorldTypeId(): Int {
     return when (this.toLowerCase()) {
-        "default" -> WorldType.Default
-        "flat" -> WorldType.Flat
-        "custom" -> WorldType.Customize
-        "empty" -> WorldType.Empty
-        else -> WorldType.valueOf(this)
+        "default" -> 1
+        "flat" -> 0
+        "custom" -> 1
+        "empty" -> 0
+        "old" -> 2
+        else -> 0
+    }
+}
+
+fun Int.toWorldType(): WorldType {
+    return when (this) {
+        1 -> WorldType.Default
+        0 -> WorldType.Flat
+        2 -> WorldType.Old
+        else -> WorldType.Default
+    }
+}
+
+fun WorldType.toStringValue(): String {
+    return when (this) {
+        WorldType.Default -> "default"
+        WorldType.Flat -> "flat"
+        WorldType.Customize -> "custom"
+        WorldType.Empty -> "empty"
+        WorldType.Old -> "old"
+    }
+}
+
+fun WorldType.toIntValue(): Int {
+    return when (this) {
+        WorldType.Default -> 1
+        WorldType.Flat -> 0
+        WorldType.Customize -> 1
+        WorldType.Empty -> 0
+        WorldType.Old -> 2
     }
 }
