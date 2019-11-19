03f89ffae9a1b2752304f89ad0dc914a969248f3
hiroki19990625 <hiroki19990625@gmail.com>
2019-11-19 21:41:24 +0900

    リファクタリング

--- src/main/kotlin/jp/katana/server/InternalServerError.kt
+++ src/main/kotlin/jp/katana/server/InternalServerError.kt
@@ -2,4 +2,4 @@ package jp.katana.server
 
 import jp.katana.i18n.I18n
 
-class InternalServerError() : Error(I18n["katana.server.error.critical"])
+class InternalServerError : Error(I18n["katana.server.error.critical"])
