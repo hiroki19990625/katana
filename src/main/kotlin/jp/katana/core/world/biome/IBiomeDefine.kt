8704838f4724b12bdaa2235a5902a8170d9d624b
hiroki199990625 <hiroki19990625@gmail.com>
2020-06-29 00:24:09 +0900

    バイオームに定義を追加

--- src/main/kotlin/jp/katana/core/world/biome/IBiomeDefine.kt
+++ src/main/kotlin/jp/katana/core/world/biome/IBiomeDefine.kt
@@ -2,6 +2,10 @@ package jp.katana.core.world.biome
 
 interface IBiomeDefine {
     val name: String
+    val blueSpores: Float
+    val whiteAsh: Float
+    val ash: Float
     val temperature: Float
+    val redSpores: Float
     val downfall: Float
 }
--- src/main/kotlin/jp/katana/server/world/biome/BiomeDefine.kt
+++ src/main/kotlin/jp/katana/server/world/biome/BiomeDefine.kt
@@ -2,5 +2,12 @@ package jp.katana.server.world.biome
 
 import jp.katana.core.world.biome.IBiomeDefine
 
-class BiomeDefine(override val name: String, override val temperature: Float, override val downfall: Float) :
-    IBiomeDefine
+class BiomeDefine(
+    override val name: String,
+    override val blueSpores: Float,
+    override val whiteAsh: Float,
+    override val ash: Float,
+    override val temperature: Float,
+    override val redSpores: Float,
+    override val downfall: Float
+) : IBiomeDefine
