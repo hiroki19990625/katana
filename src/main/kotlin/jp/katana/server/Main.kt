1d5601804e464f3351c1c944e1eae9c6d18b73f6
hiroki199990625 <hiroki19990625@gmail.com>
2020-06-30 16:06:58 +0900

    バッチと例外処理

--- src/main/kotlin/jp/katana/server/Main.kt
+++ src/main/kotlin/jp/katana/server/Main.kt
@@ -2,28 +2,43 @@ package jp.katana.server
 
 import jp.katana.client.Client
 import jp.katana.i18n.I18n
-import org.apache.logging.log4j.Logger
 
 class Main {
     companion object {
-        private var logger: Logger? = null
-
         @JvmStatic
         fun main(args: Array<String>) {
             if (args.isEmpty()) {
                 val server = Server()
                 server.logger.info(I18n["katana.selector.startServer"])
-                server.start()
+
+                try {
+                    server.start()
+                } catch (e: Throwable) {
+                    server.logger.fatal("", e)
+                    readLine()
+                }
             } else {
                 val type = args[0]
                 if (type == "client") {
                     val client = Client()
-                    client.logger.info(I18n["katana.selector.startServer"])
-                    client.start()
+                    client.logger.info(I18n["katana.selector.startClient"])
+
+                    try {
+                        client.start()
+                    } catch (e: Throwable) {
+                        client.logger.fatal("", e)
+                        readLine()
+                    }
                 } else if (type == "server") {
                     val server = Server()
                     server.logger.info(I18n["katana.selector.startServer"])
-                    server.start()
+
+                    try {
+                        server.start()
+                    } catch (e: Throwable) {
+                        server.logger.fatal("", e)
+                        readLine()
+                    }
                 }
             }
         }
