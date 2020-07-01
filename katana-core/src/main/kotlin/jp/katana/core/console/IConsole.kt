e72401bc0e6792889d1ac6ad39344bbec36b3c71
hiroki199990625 <hiroki19990625@gmail.com>
2020-07-01 23:54:24 +0900

    fix インタフェースの定義

--- katana-core/src/main/kotlin/jp/katana/core/console/IConsole.kt
+++ katana-core/src/main/kotlin/jp/katana/core/console/IConsole.kt
@@ -5,6 +5,12 @@ package jp.katana.core.console
  */
 interface IConsole {
     /**
+     * コマンドを実行します。
+     * @param command コマンド
+     */
+    fun runCommand(command: String?)
+
+    /**
      * 入力されたコマンドを読み取ります。
      * @return 読み取られたコマンド
      */
